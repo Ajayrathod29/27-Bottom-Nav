878c1259f28928a43483887ad69bb7094ae3decd
Ajay Rathod <ajayrathod1137@gmail.com>
2023-03-23 01:55:15 +0530

    initial commit

---
+++ app/src/main/java/com/example/bottomnevigationview/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.bottomnevigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        supportActionBar?.hide()
+        val bottonNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        replaceWithFragment(Home())
+
+        bottonNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.item1 -> replaceWithFragment(Home())
+                R.id.item2 -> replaceWithFragment(Search())
+                R.id.item3 -> replaceWithFragment(Profile())
+                else -> {
+                }
+            }
+            true
+        }
+    }
+    private fun replaceWithFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
